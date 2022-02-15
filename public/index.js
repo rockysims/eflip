7c73f68a8886fd81ced4c5d51aebf18315179f0e
rockysims <rocky.sims@gmail.com>
2022-02-14 21:31:32 -0800

    Multiple tweaks and improvements to flip report code.

--- public/index.js
+++ public/index.js
@@ -1,8 +1,13 @@
-const THE_FORGE_REGION_ID = 10000002;
-const DAYS_CONSIDERED = 30;
+const JITA_REGION_ID = 10000002; //The Forge
+const AMARR_REGION_ID = 10000043; //Domain
+const DODIXIE_REGION_ID = 10000032; //Sinq Laison
+const RENS_REGION_ID = 10000030; //Heimatar
+// const HEK_REGION_ID = 10000042; //Metropolis
+const THE_REGION_ID = DODIXIE_REGION_ID;
+const DAYS_CONSIDERED = 20//30;
 const STEP_SIZE = 500;
-const SELL_TAX = 0.11;
-const BUY_TAX = 0.6;
+const SELL_TAX = 0.08 + 0.0233;
+const BUY_TAX = 0.01;
 
 const saveJson = async (path, data) => {
 	await fetch(`/file/${path}`, {
@@ -24,6 +29,34 @@ const loadJson = async (path) => {
 	);
 };
 
+const fetchWithRetries = async (url) => {
+	for (let retries = 0; retries < 5; retries++) {
+		const fetchedData = await fetch(url, {
+			method: 'get'
+		}).then(res => {
+			const data = res.json();
+			if (data.error) {
+				console.log('GET failed. data.error: ', data.error);
+				return null;
+			} else {
+				return data;
+			}
+		}, async error => {
+			console.log('GET failed. error.response.status: ', error.response.status);
+			return null;
+		});
+		
+		if (fetchedData !== null) {
+			return fetchedData;
+		} else {
+			console.log(`retrying ${url}`);
+		}
+	}
+
+	console.error('fetchWithRetries() exhausted retry limit');
+	return null;
+};
+
 const getOrFetch = async url => {
 	const path = `${url.replace(/[^a-zA-Z0-9]+/g, '')}.json`;
 	try {
@@ -33,10 +66,7 @@ const getOrFetch = async url => {
 		console.log('getOrFetch caught reason: ', reason)
 	}
 
-	return fetch(url, {
-		method: 'get'
-	}).then(async res => {
-		const data = await res.json();
+	return fetchWithRetries(url).then(async data => {
 		await saveJson(path, data);
 		return data;
 	}, async error => {
@@ -59,7 +89,7 @@ const getTypeIds = async (regionId) => {
 	});
 };
 
-const getTypeNameById = async () => {
+const loadTypeNameById = async () => {
 	const typeData = await fetch('invTypes.json', {
 		method: 'get'
 	}).then(res => res.json());
@@ -76,30 +106,29 @@ const getDays = async (regionId, typeId) => {
 	const url = `https://esi.evetech.net/latest/markets/${regionId}/history/?datasource=tranquility&type_id=${typeId}`;
 	const days = await getOrFetch(url);
 	return Array.isArray(days)
-		? days.slice(-1 * DAYS_CONSIDERED)
+		? days
 		: [];
 };
 
 const getOrders = async (regionId, typeId) => {
 	const url = `https://esi.evetech.net/latest/markets/${regionId}/orders/?datasource=tranquility&type_id=${typeId}`;
-	return await getOrFetch(url);
+	const orders = await getOrFetch(url);
+	return Array.isArray(orders)
+		? orders
+		: [];
 };
 
+const roundMils = (amount) => {
+	return Math.round(amount / (1000000/100)) / 100;
+};
+
+const nowMoment = moment();
 const getItemReport = async (regionId, typeId) => {
 	try {
 		const days = await getDays(regionId, typeId);
 		const orders = await getOrders(regionId, typeId);
 		
-		const recentOrders = orders.filter(order => {
-			const issued = moment(order.issued);
-			const hoursOld = moment().diff(issued, 'hour');
-			return hoursOld < 24;
-		});
-		const recentSellOrders = recentOrders.filter(order => order.is_buy_order === false);
-		const recentBuyOrders = recentOrders.filter(order => order.is_buy_order === true);
-		const activeFlippers = Math.max(recentSellOrders.length, recentBuyOrders.length);
-
-		const overallAverage = days.map(day => day.average).reduce((a, c) => a + c, 0) / days.length;
+		const overallAverage = days.map(day => day.average).reduce((a, c) => a + c, 0) / DAYS_CONSIDERED;
 	
 		let totalFlipProfit = 0;
 		let totalFlipVolume = 0;
@@ -107,14 +136,14 @@ const getItemReport = async (regionId, typeId) => {
 		let sellRevenues = [];
 		const profitPerFlipList = [];
 		for (let day of days) {
-			if (days.length < DAYS_CONSIDERED) break;
+			if (nowMoment.diff(day.date, 'd') > DAYS_CONSIDERED) continue;
 			if (day.highest === day.lowest) continue;
 			if (day.highest > overallAverage * 10) continue; //crazy over priced so ignore
 			const lowFrac = (day.highest - day.average) / (day.highest - day.lowest);
 			const highFrac = 1 - lowFrac;
 			const lowVolume = lowFrac * day.volume;
 			const highVolume = highFrac * day.volume;
-			const flipVolume = Math.floor(Math.min(lowVolume, highVolume) / (activeFlippers + 1));
+			const flipVolume = Math.floor(Math.min(lowVolume, highVolume));
 			const sellRevenue = day.highest * (1 - SELL_TAX);
 			const buyCost = day.lowest * (1 + BUY_TAX);
 			const profitPerFlip = sellRevenue - buyCost;
@@ -127,20 +156,49 @@ const getItemReport = async (regionId, typeId) => {
 			}
 		}
 
+		//calc profitPerFlipNow
+		const buyOrders = orders.filter(order => order.is_buy_order === true);
+		const bestBuyOrder = buyOrders.reduce((order, bestBuyOrder) => {
+			if (order.price > bestBuyOrder.price) bestBuyOrder = order;
+			return bestBuyOrder;
+		}, buyOrders[0] || null);
+		const sellOrders = orders.filter(order => order.is_buy_order === false);
+		const bestSellOrder = sellOrders.reduce((order, bestSellOrder) => {
+			if (order.price < bestSellOrder.price) bestSellOrder = order;
+			return bestSellOrder;
+		}, sellOrders[0] || null);
+		const buyCostNow = (bestBuyOrder?.price || 0) * (1 + BUY_TAX);
+		const sellRevenueNow = (bestSellOrder?.price || 0) * (1 - SELL_TAX);
+		const profitPerFlipNow = sellRevenueNow - buyCostNow;
+
+		//calc activeFlippers
+		const daysPerFlip = DAYS_CONSIDERED / totalFlipVolume;
+		const recentOrders = orders.filter(order => {
+			const issued = moment(order.issued);
+			const hoursOld = moment().diff(issued, 'hour');
+			return hoursOld < 24 * Math.max(1, daysPerFlip);
+		});
+		const recentSellOrders = recentOrders.filter(order => order.is_buy_order === false);
+		const recentBuyOrders = recentOrders.filter(order => order.is_buy_order === true);
+		const activeFlippers = Math.max(recentSellOrders.length, recentBuyOrders.length);
+
 		const buyCostAvg = buyCosts.reduce((a, c) => a + c, 0) / buyCosts.length;
 		const sellRevenueAvg = sellRevenues.reduce((a, c) => a + c, 0) / sellRevenues.length;
 		const profitPerFlipAvg = profitPerFlipList.reduce((a, c) => a + c, 0) / profitPerFlipList.length;
-		const dailyFlipProfit = totalFlipProfit / days.length;
+		const totalDailyFlipProfit = totalFlipProfit / DAYS_CONSIDERED;
+		const dailyFlipProfit = totalDailyFlipProfit / (activeFlippers + 1);
 		
-		const buyCostAvgMil = Math.round(buyCostAvg / (1000000/100)) / 100;
-		const sellRevenueAvgMil = Math.round(sellRevenueAvg / (1000000/100)) / 100;
+		const buyCostAvgMil = buyCostAvg < 10000 ? buyCostAvg : Math.round(buyCostAvg / (1000000/100)) / 100;
+		const sellRevenueAvgMil = sellRevenueAvg < 10000 ? sellRevenueAvg : Math.round(sellRevenueAvg / (1000000/100)) / 100;
 		return {
-			totalFlipVolume: Math.floor(totalFlipVolume),
+			dailyFlipVolume: totalFlipVolume / DAYS_CONSIDERED,
+			// availableFlipVolume: Math.floor(totalFlipVolume / (activeFlippers + 1)),
 			buyCostAvgMil,
 			sellRevenueAvgMil,
 			activeFlippers,
-			profitPerFlipAvgMil: Math.round(profitPerFlipAvg / (1000000/100)) / 100,
-			dailyFlipProfitMil: Math.round(dailyFlipProfit / (1000000/100)) / 100
+			profitPerFlipNowMil: roundMils(profitPerFlipNow),
+			profitPerFlipAvgMil: roundMils(profitPerFlipAvg),
+			dailyFlipProfitMil: roundMils(dailyFlipProfit)
 		};
 	} catch (reason) {
 		console.warn(reason);
@@ -148,14 +206,36 @@ const getItemReport = async (regionId, typeId) => {
 	}
 };
 
+let typeNameById = null;
+const getTypeName = async typeId => {
+	if (typeNameById === null) {
+		typeNameById = await loadTypeNameById(typeId);
+	}
+
+	if (!typeNameById[typeId]) {
+		const url = `https://esi.evetech.net/latest/universe/types/${typeId}/?datasource=tranquility&language=en`;
+		const type = await getOrFetch(url);
+		typeNameById[typeId] = type.name;
+	}
+
+	return typeNameById[typeId];
+};
+
 document.addEventListener('DOMContentLoaded', async () => {
 	const outputElem = document.querySelector('.outputDiv');
 
 	outputElem.innerHTML = 'Starting...';
-	const typeIds = await getTypeIds(THE_FORGE_REGION_ID);
+	
+	
+	
+	const typeIds = (await getTypeIds(THE_REGION_ID)); //TODO: remove the ".slice(0, 1000)" part
+	// const typeIds = [61869];
+
+
+
 	outputElem.innerHTML = 'typeIds.length: ' + typeIds.length;
 
-	const typeNameById = await getTypeNameById();
+	// const typeNameById = await getTypeNameById();
 
 	outputElem.innerHTML = `Processing`;
 
@@ -163,7 +243,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 	for (let step = 0; step * STEP_SIZE < typeIds.length; step++) {
 		const promises = [];
 		for (let typeId of typeIds.slice(step * STEP_SIZE, (step + 1) * STEP_SIZE)) {
-			const reportPromise = getItemReport(THE_FORGE_REGION_ID, typeId);
+			const reportPromise = getItemReport(THE_REGION_ID, typeId);
 			reportPromise.then(itemReport => itemReportByTypeId[typeId] = itemReport);
 			promises.push(reportPromise);
 		}
@@ -182,6 +262,8 @@ document.addEventListener('DOMContentLoaded', async () => {
 	outputElem.innerHTML = `Ready`;
 
 	let html = '';
+	html += `Region: ${THE_REGION_ID}<br/>`;
+	html += '<br/>';
 	for (let typeId of typeIdsOrderedByProfitDesc) {
 		// const typeName = typeNameById[typeId];
 		// if (typeName.includes('Men\'s') || typeName.includes('Women\'s') || typeName.includes('SKIN')) continue;
@@ -189,23 +271,27 @@ document.addEventListener('DOMContentLoaded', async () => {
 		const itemReport = itemReportByTypeId[typeId];
 		if (!itemReport) continue;
 		if (itemReport.dailyFlipProfitMil < 1 || itemReport.totalFlipVolume === 0) {
-			console.log(`${typeId} not profitable`);
+			// console.log(`${typeId} not profitable`);
 			continue;
 		}
-		if (itemReport.totalFlipVolume < 5) {
+		if (itemReport.profitPerFlipNowMil < itemReport.profitPerFlipAvgMil * 0.8) {
+			console.log(`${typeId} margin crashed`);
+			continue;
+		}
+		if (itemReport.totalFlipVolume < 3) {
 			console.log(`${typeId} volume too low`);
+			continue;
+		}
+		if (itemReport.sellRevenueAvgMil < itemReport.buyCostAvgMil * 1.25) {
+			console.log('margin too slim');
 			continue;
 		}
 		if (itemReport.buyCostAvgMil > 100) {
 			console.log('price too high');
 			continue;
 		}
-		if (itemReport.sellRevenueAvgMil < itemReport.buyCostAvgMil * 2) {
-			console.log('margin too slim');
-			continue;
-		}
 		html += '<div>';
-		html += 	`${typeNameById[typeId]} (${typeId})`;
+		html += 	`${await getTypeName(typeId)} (${typeId})`;
 		html += '</div>';
 		html += '<div>';
 		html += 	`${itemReport.dailyFlipProfitMil}`;
